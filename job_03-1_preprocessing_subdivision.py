2ad2ad2832493c1ec7527004587a84ce059d8ada
jabim <sabin20030720@gmail.com>
2022-07-27 12:02:26 +0900

    preprocessing subdivision name refactor

--- job_03-1_preprocessing_subdivision.py
+++ job_03-1_preprocessing_subdivision.py
@@ -11,9 +11,9 @@ import glob
 df = pd.DataFrame()
 data_paths = glob.glob('./wanted/clear/*')
 data_paths = sorted(data_paths)
-job_list = ['웹', '서버', '프론트엔드', '소프트웨어', '자바', '안드로이드', 'iOS', 'Nodejs', 'C++', '데이터엔지니어', 'DevOps', '파이썬', '시스템관리자', '머신러닝엔지니어',
+job_list = ['웹', '서버', '프론트엔드', '소프트웨어', '자바', '안드로이드', 'iOS', 'Nodejs', 'C++', '데이터엔지니어', '파이썬', 'DevOps', '시스템관리자', '머신러닝엔지니어',
                 '데이터사이언티스트', '빅데이터엔지니어', 'QA', '기술지원', '개발매니저', '보안엔지니어', '프로덕트매니저', '블록체인엔지니어', 'PHP개발자', '임베디드개발자', '웹퍼블리셔',
-                '크로스플랫폼', '하드웨어엔지니어', 'DBA', 'NET개발자', '영상음성엔지니어', 'CTO', '그래픽스엔지니어', 'VR엔지니어', 'BI 엔지니어', 'ERP전문가', '루비온레일즈개발자',
+                '하드웨어엔지니어', '크로스플랫폼', 'DBA', 'NET개발자', '영상음성엔지니어', '그래픽스엔지니어', 'CTO', 'VR엔지니어', 'BI엔지니어', 'ERP전문가', '루비온레일즈개발자',
                 'CIO']
 cnt = 0
 for path in data_paths:
@@ -98,14 +98,15 @@ for path in data_paths:
                     words.append(word)
         cleaned_sentence = ' '.join(words)
         first_cleaned_welfare.append(cleaned_sentence)
-    first_cleaned_all = []
-    for i in range(len(first_cleaned_welfare)):
-        first_cleaned_all.append(first_cleaned_welfare[i] + first_cleaned_works[i])
-    df['first_cleaned_all'] = first_cleaned_all
+    # first_cleaned_all = []
+    # for i in range(len(first_cleaned_welfare)):
+    #     first_cleaned_all.append(first_cleaned_welfare[i] + first_cleaned_works[i])
+    df['first_cleaned_welfare'] = first_cleaned_welfare
+    df['first_cleaned_works'] = first_cleaned_works
     # df['first_cleaned_welfares'] = first_cleaned_welfare
     df.dropna(inplace=True)
 
-    df.to_csv(f'./wanted/clear_data/{job_list[cnt]}_data_preprocessing_02.csv', index=False)
+    df.to_csv(f'./wanted/preprocessing_data/{job_list[cnt]}_preprocessing_01.csv', index=False)
     print(df)
     df.info()
     cnt += 1
