9570865565ee4bb520c6af5bf732bd8f2539e710
jabim <sabin20030720@gmail.com>
2022-07-28 15:33:46 +0900

    concat recommendation code add

---
+++ job09_concat_recommendation.py
@@ -0,0 +1,72 @@
+import pandas as pd
+from sklearn.metrics.pairwise import linear_kernel
+import pickle
+from konlpy.tag import Okt
+import re
+from gensim.models import Word2Vec
+import numpy as np
+from sklearn.feature_extraction.text import TfidfVectorizer
+from scipy.io import mmwrite, mmread
+import glob
+
+def getRecommendation(cosin_sim):
+    simScore = list(enumerate(cosin_sim[-1]))
+    simScore = sorted(simScore, key=lambda x:x[1], reverse=True)
+    simScore = simScore[:11]
+    movieIdx = [i[0] for i in simScore]
+    recMovieList = df.iloc[movieIdx, 2]
+    return recMovieList
+            # 머신러닝엔지니어[https://www.wanted.co.kr/wd/53925]
+df_reviews = pd.read_csv('./wanted/clear_data/13_머신러닝엔지니어_data_scaler.csv')
+user_data = ['https://www.wanted.co.kr/wd/75640', 'https://www.wanted.co.kr/wd/114692', 'https://www.wanted.co.kr/wd/56091', 'https://www.wanted.co.kr/wd/109068', 'https://www.wanted.co.kr/wd/111447']
+df_머신러닝엔지니어 = pd.read_csv('./wanted/clear_data/13_머신러닝엔지니어_data_scaler.csv')
+df_데이터사이언티스트 = pd.read_csv('./wanted/clear_data/14_데이터사이언티스트_data_scaler.csv')
+df_빅데이터엔지니어 = pd.read_csv('./wanted/clear_data/15_빅데이터엔지니어_data_scaler.csv')
+
+
+df_머신러닝엔지니어.dropna(inplace=True)
+df_머신러닝엔지니어.reset_index(inplace=True)
+
+df_데이터사이언티스트.dropna(inplace=True)
+df_데이터사이언티스트.reset_index(inplace=True)
+
+df_빅데이터엔지니어.dropna(inplace=True)
+df_빅데이터엔지니어.reset_index(inplace=True)
+
+data_DB = [df_머신러닝엔지니어, df_빅데이터엔지니어, df_데이터사이언티스트]
+
+df = pd.DataFrame()
+for path in data_DB:
+    df = pd.concat([df, path], ignore_index=True)
+df.drop_duplicates(['page_url'], inplace=True)
+
+df_mark = pd.DataFrame()
+for path in user_data:
+    data_ = df[df['page_url']==path]
+    print(data_)
+    df_mark = pd.concat([df_mark, data_], ignore_index=True)
+df.drop_duplicates(inplace=True)
+print(df)
+print(df_mark['page_url'])
+
+Tfidf_welfare = TfidfVectorizer(sublinear_tf=True)
+Tfidf_matrix_welfare = Tfidf_welfare.fit_transform(df['first_cleaned_welfare'])
+
+Tfidf_work = TfidfVectorizer(sublinear_tf=True)
+Tfidf_matrix_work = Tfidf_work.fit_transform(df['first_cleaned_works'])
+print(Tfidf_matrix_welfare)
+
+Tfidf = TfidfVectorizer(sublinear_tf=True)
+Tfidf_matrix_mk_welfare = Tfidf.fit_transform(df['first_cleaned_welfare'])
+Tfidf_matrix_mk_work = Tfidf.fit_transform(df['first_cleaned_works'])
+
+print(Tfidf_matrix_mk_work)
+print(Tfidf_matrix_mk_welfare)
+exit()
+cosine_sim1 = linear_kernel(Tfidf_matrix_welfare[-1], Tfidf_matrix_welfare)
+cosine_sim2 = linear_kernel(Tfidf_matrix_work[-1], Tfidf_matrix_work)
+
+cosine_sim = cosine_sim1 * 0.25 + cosine_sim2 * 0.6 + np.array(df['scaler_money'], np.float) * 0.15
+
+recommendation = getRecommendation(cosine_sim)
+print(recommendation[1:11])
