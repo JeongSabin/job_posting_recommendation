795f7482858508caa4d8df3ec2c7602adad9f5b8
jabim <sabin20030720@gmail.com>
2022-08-09 17:09:53 +0900

    DB connect test code push(grid layer error fix)

--- DjangoSJ2/theme/views.py
+++ DjangoSJ2/theme/views.py
@@ -21,7 +21,7 @@ def bookmarked_items(request):
 
 def category(request):
     all_develop_manager_data = develop_manager_data.objects.all().order_by("page_url")  # 모든 데이터 조회, 내림차순(-표시) 조회
-    paginator = Paginator(all_develop_manager_data, 12)
+    paginator = Paginator(all_develop_manager_data, 1700)
     page = int(request.GET.get('page', 1))
     develop_manager_data_list = paginator.get_page(page)
     return render(request, 'category.html', {'develop_manager_data_list': develop_manager_data_list})
